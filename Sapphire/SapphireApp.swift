35422bcd60c89f5bbbb5bd357b53d76d0d6f9437
srujan <srujanmurthy2006@gmail.com>
2023-04-27 21:56:48 -0500

    Initial Commit

---
+++ Sapphire/SapphireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SapphireApp.swift
+//  Sapphire
+//
+//  Created by Surya kiran on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SapphireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
