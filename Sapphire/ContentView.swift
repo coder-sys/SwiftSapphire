b8d2845f580759aff86711ec5abfdef02cbed919
coder-sys <unix99493@gmail.com>
2023-05-19 05:56:01 -0500

    Added API transcription

--- Sapphire/ContentView.swift
+++ Sapphire/ContentView.swift
@@ -8,44 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var viewmodel:Viewmodel = Viewmodel(transccript: "hello world")
+    @State var viewmodel:Viewmodel = Viewmodel(transcript: "hi world")
     @State var items:Model = Model(txt: "")
     @State var score:Double = Double(0.0)
+    @State private var isLoading = false
+    @State private var errorMessage = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor).onTapGesture {
+                    viewmodel.fetchData()
                     print(viewmodel.EvaluateTranscript())
 
                 }
         }
         .padding()
-        Text(items.txt).onAppear{
-            fetchData()
-        }
     }
-    func fetchData() {
-      let api = "http://127.0.0.1:5000"
-      guard let url = URL(string: api) else { return }
-        
-        
-      URLSession.shared.dataTask(with: url) { (data, response, error) in
-        do {
-           if let data = data {
-             let result = try JSONDecoder().decode(Model.self, from: data)
-             DispatchQueue.main.async {
-                 self.items = result
-                 
-           }
-           } else {
-             print("No data")
-           }
-        } catch (let error) {
-           print(error.localizedDescription)
-        }
-       }.resume()
-    }
+    
+
     
     struct Model: Decodable {
         let txt:String
