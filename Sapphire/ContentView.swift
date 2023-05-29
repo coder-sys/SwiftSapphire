6342a54cec33c68b71eb72ad9ab4ac66adb5c2c9
coder-sys <71105356+coder-sys@users.noreply.github.com>
2023-05-29 17:51:37 -0500

    Update ContentView.swift

--- Sapphire/ContentView.swift
+++ Sapphire/ContentView.swift
@@ -8,32 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var viewmodel:Viewmodel = Viewmodel(transcript: "hi world")
-    @State var items:Model = Model(txt: "")
-    @State var score:Double = Double(0.0)
-    @State private var isLoading = false
-    @State private var errorMessage = ""
+    @StateObject private var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor).onTapGesture {
-                    viewmodel.fetchData()
-                    print(viewmodel.EvaluateTranscript())
-
-                }
+            if let txt = viewModel.items?.txt {
+                
+                let tokeniser:Tokenization = Tokenization(corpus: "hi im srujan, how do you dp".lowercased())
+                
+                var sentTokenized = tokeniser.tokenizeTextIntoSentences(string:tokeniser.text,byDelimiter:".");
+                
+                let sentTokens = tokeniser.sentTokenStrip(sentTokenized)
+                let tokens = tokeniser.detectWordTokens(in:sentTokens)
+                let tfidf = TFIDF(tokens: tokeniser.tokens, sentTokens:sentTokens)
+                let assessor:KeyWordAssesor = KeyWordAssesor(statistics:tfidf.collection)
+                var collection:TFIDF.DATA = assessor.eliminateIteration(in: tfidf.collection)
+                let finalScore:FinalScoreCalculator = FinalScoreCalculator(statistics: collection)
+                Text(String(finalScore.score))
+            } else {
+                Text("No transcription available")
+            }
+            
+            Button("Fetch Data") {
+                viewModel.fetchData()
+            }
         }
-        .padding()
     }
     
-
-    
-    struct Model: Decodable {
-        let txt:String
-    }
     struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+    static var previews: some View {
+        ContentView()
     }
+    }
+    
+    //let tfidf = TFIDF(tokens: tokeniser.tokens, sentTokens: tokeniser.sentTokenized)
+    //let assessor:KeyWordAssesor = KeyWordAssesor(statistics:tfidf.collection)
+    //var collection:TFIDF.DATA = assessor.eliminateIteration(in: tfidf.collection)
+    //let finalScore:FinalScoreCalculator = FinalScoreCalculator(statistics: collection)
 }
+//struct ContentView_Previews: PreviewProvider {
+//static var previews: some View {
+//    ContentView()
+//}
+//}
+
+//          var tokeniser:Tokenization = Tokenization(corpus: txt.lowercased())
+//         let tfidf = TFIDF(tokens: tokeniser.tokens, sentTokens: tokeniser.sentTokenized)
+//                let assessor:KeyWordAssesor = KeyWordAssesor(statistics:tfidf.collection)
+//                var collection:TFIDF.DATA = assessor.eliminateIteration(in: tfidf.collection)
+ //              collection = assessor.qualify(collection)
+//               print(collection.distrib.count,collection.tfidf.count,collection.tokens.count)
+  //             let finalScore:FinalScoreCalculator = FinalScoreCalculator(statistics: collection)
