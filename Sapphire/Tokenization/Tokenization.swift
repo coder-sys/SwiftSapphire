aae8118c2d99667684efe291ff76350a4296b307
coder-sys <71105356+coder-sys@users.noreply.github.com>
2023-05-29 17:53:03 -0500

    Update Tokenization.swift

--- Sapphire/Tokenization/Tokenization.swift
+++ Sapphire/Tokenization/Tokenization.swift
@@ -7,27 +7,13 @@
 
 import Foundation
 class Tokenization{
-    @Published var text:String
-    @Published var tokens:[String]
-    @Published var sentTokenized:[String]
+    @Published private(set) var text:String
+    @Published private(set) var tokens:[String]
+    @Published private(set) var sentTokenized:[String]
     
     
-    func tokenizeCorpus(text:String){
-        let tagger = NSLinguisticTagger(tagSchemes: [.tokenType], options: 0)
-        tagger.string = text
-        
-        let range = NSRange(location: 0, length: text.utf16.count)
-        let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace]
-        tagger.enumerateTags(in: range, unit: .word, scheme: .tokenType, options: options) { _, tokenRange, _ in
-            let word = (text as NSString).substring(with: tokenRange)
-            DispatchQueue.main.async {
-                self.text = word
-                self.tokens.append(word)
-          }
-            
-            
-        }
-    
+    func tokenizeCorpus(_ inputString: String, delimiter: Character) -> [String] {
+        return inputString.split(separator: delimiter).map { String($0) }
     }
      func sentTokenStrip(_ sentTokens:Array<String>)->Array<String>{
         var newArr:Array<String> = []
@@ -37,14 +23,19 @@ class Tokenization{
         }
         return newArr
     }
-    func detectWordTokens(in sentTokens:Array<String>){
+    func detectWordTokens(in sentTokens:Array<String>)->[String]{
         let attached = sentTokens.joined(separator: " ")
-        tokenizeCorpus(text:attached)
+        print("attached is \(attached)")
+        return tokenizeCorpus(attached,delimiter: " ")
     }
     func tokenizeTextIntoSentences(string: String, byDelimiter delimiter: String) -> [String] {
         return string.split(separator: delimiter).map { String($0) }
     }
-
+    func tokenization()->[String]{
+        sentTokenized = tokenizeTextIntoSentences(string:text,byDelimiter:".");
+        sentTokenized = sentTokenStrip(sentTokenized)
+        return detectWordTokens(in:sentTokenized)
+    }
     init(corpus:String){
         text = String();
         text = corpus;
@@ -52,7 +43,6 @@ class Tokenization{
         sentTokenized = Array<String>();
         sentTokenized = tokenizeTextIntoSentences(string:text,byDelimiter:".");
         sentTokenized = sentTokenStrip(sentTokenized)
-        detectWordTokens(in:sentTokenized)
-        print("tokens are ",tokens)
+        tokens = detectWordTokens(in:sentTokenized)
     }
 }
